452f122ad6d29bbb7ed1e438682ff45d05b2d3e9
Adelsayed411 <adel15omar1@gmail.com>
2024-04-20 20:34:54 +0200

    Update insertion Sotr

---
+++ InsertionSort.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Student
+{
+public:
+    string id;
+    string name;
+    double gpa;
+
+    Student(string id, string name, double gpa)
+    {
+        this->id = id;
+        this->name = name;
+        this->gpa = gpa;
+    }
+
+    friend void Insertion_sortByGpa(vector<Student> &students);
+};
+
+void Insertion_sortByGpa(vector<Student> &students)
+{
+    for (int i = 1, j; i < students.size(); i++)
+    {
+        Student tmp = students[i];
+
+        for (j = i; j > 0 && tmp.gpa < students[j - 1].gpa; j--)
+            students[j] = students[j - 1];
+        students[j] = tmp;
+    }
+}
+void Insertion_sortByname(vector<Student> &students)
+{
+    for (int i = 1, j; i < students.size(); i++)
+    {
+        Student tmp = students[i];
+
+        for (j = i; j > 0 && tmp.name < students[j - 1].name; j--)
+            students[j] = students[j - 1];
+        students[j] = tmp;
+    }
+}
+
+int main()
+{
+    // example to check code
+    // Student adel("20220173", "Adel", 3.32);
+    // Student zeyad("20220146", "zeyad", 3.45);
+    // vector<Student> students = {zeyad, adel};
+    // Insertion_sortByGpa(students);
+    // cout << "*By GPA*\n";
+    // for (auto x : students)
+    //     cout << x.name << '\n'
+    //          << x.id << '\n'
+    //          << x.gpa << "\n\n";
+
+    // cout << "*By NAME*\n";
+    // Insertion_sortByname(students);
+    // for (auto x : students)
+    // cout << x.name << '\n'
+    //      << x.id << '\n'
+    //      << x.gpa << "\n\n";
+    return 0;
+}
