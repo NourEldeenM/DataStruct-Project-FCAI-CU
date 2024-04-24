6638bb76c15ef656b24ee9aeee4240f777146b84
Youssef Abdulmoneim Sayed Abu serie <140813950+youssefabdulmoneim@users.noreply.github.com>
2024-04-24 21:22:59 +0200

    Update Student.cpp

--- Student.cpp
+++ Student.cpp
@@ -13,19 +13,18 @@ Student::Student() {
 }
 
 
-template<typename T, typename Compare>
-void Insertion_sort(vector<Student> &items, Compare comp) {
+template<typename Student, typename Compare>
+void Insertion_sort(vector <Student> &items, Compare comp) {
     for (int i = 1, j; i < items.size(); i++) {
-        T tmp = items[i];
-
-        for (j = i; j > 0 && comp(tmp, items[j - 1]); j--)
+        Student tmp = items[i];
+        for (j = i; j >= 0 && comp(tmp, items[j - 1]); j--)
             items[j] = items[j - 1];
         items[j] = tmp;
     }
 }
 
 template<typename student, typename Compare>
-void Selection_sort(vector<student> &items, Compare comp) {
+void Selection_sort(vector <student> &items, Compare comp) {
     for (int i = 0; i < items.size() - 1; i++) {
         int minIndex = i;
         for (int j = i + 1; j < items.size(); j++) {
@@ -40,8 +39,7 @@ void Selection_sort(vector<student> &items, Compare comp) {
 }
 
 template<typename student, typename Compare>
-
-void bubbleSort(vector<Student> &students, Compare comp) {
+void bubbleSort(vector <Student> &students, Compare comp) {
     int n = students.size();
     int i, j;
     bool swapped;
@@ -56,4 +54,33 @@ void bubbleSort(vector<Student> &students, Compare comp) {
         if (!swapped)
             break;
     }
+}
+
+template<typename Student, typename Compare>
+void quickSort(vector <Student> &students, Compare comp, int i, int j) {
+    if (i >= j) return;
+    Student pivot = students[(i + j) / 2];
+    //partition
+    int left = i, right = j;
+    while (left <= right) {
+        while (comp(students[left], pivot))
+            left++;
+        while (comp(pivot, students[right]))
+            right--;
+        if (left <= right) {
+            swap(students[left], students[right]);
+            left++;
+            right--;
+        }
+    };
+    if (i < right)
+        quickSort(students, comp, i, right);
+    if (left < j)
+        quickSort(students, comp, left, j);
+}
+
+//Wrapper function
+template<typename Student, typename Compare>
+void quickSort(vector <Student> &students, Compare comp) {
+    quickSort(students, comp, 0, students.size() - 1);
 }
