ce6ce9714e80bfb070d19434faa39c7ef200ef60
Zeyad Mohamed <zeadmohamed522@gmail.com>
2024-05-11 23:16:35 +0300

    Assignment 1

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: E:/Semester 4/Data Structures/Assignments/Assignment 1/Repo/DataStruct-Project-FCAI-CU
-# Build directory: E:/Semester 4/Data Structures/Assignments/Assignment 1/Repo/DataStruct-Project-FCAI-CU/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
