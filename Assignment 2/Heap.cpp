27692b770c297741643fc5572f6149b5835e4521
Youssef Abdulmoneim Sayed Abu serie <140813950+youssefabdulmoneim@users.noreply.github.com>
2024-05-17 23:52:08 +0300

    Update Heap.cpp

--- Assignment 2/Heap.cpp
+++ Assignment 2/Heap.cpp
@@ -1,39 +1,48 @@
 #include "Heap.h"
 
+struct CompareByName {
+    bool operator()(const Item &a, const Item &b) const {
+        return a.getName() < b.getName();
+    }
+};
+
+struct CompareByCategory {
+    bool operator()(const Item &a, const Item &b) const {
+        return a.getCategory() < b.getCategory();
+    }
+};
+
+struct CompareByPrice {
+    bool operator()(const Item &a, const Item &b) const {
+        return a.getPrice() < b.getPrice();
+    }
+};
+
 template<typename Comparator>
 Heap<Comparator>::Heap(Comparator comp, bool minHeap) : compare(comp), isMinHeap(minHeap) {}
 
 template<typename Comparator>
-void Heap<Comparator>::changeHeapType() {
-    isMinHeap = !isMinHeap;
-    buildHeap();
-}
-
-template<typename Comparator>
-void Heap<Comparator>::addItem(const Item &item) {
-    heap.push_back(item);
-    heapifyUp(size() - 1);
-}
-
-template<typename Comparator>
-void Heap<Comparator>::removeItem() {
-    if (size() == 0)
-        return;
-    heap[0] = heap.back();
-    heap.pop_back();
-    heapifyDown(0);
-}
-
-template<typename Comparator>
-int Heap<Comparator>::size() {
-    return heap.size();
-}
-
-template<typename Comparator>
-void Heap<Comparator>::displayItems() {
+void Heap<Comparator>::displaySortedByPrice(bool ascending) {
     if (heap.empty())
         return void(cout << "Nothing to display. \n");
-    for (auto &item: heap) {
+    vector<Item> Sorted = heap;
+    sort(Sorted.begin(), Sorted.end(), [ascending](Item a, Item b) {
+        return ascending ? a.getPrice() < b.getPrice() : a.getPrice() > b.getPrice();
+    });
+    for (const auto &item: Sorted) {
+        item.print();
+    }
+}
+
+template<typename Comparator>
+void Heap<Comparator>::displaySortedByName(bool ascending) {
+    if (heap.empty())
+        return void(cout << "Nothing to display. \n");
+    vector<Item> Sorted = heap;
+    sort(Sorted.begin(), Sorted.end(), [ascending](Item a, Item b) {
+        return ascending ? a.getName() < b.getName() : a.getName() > b.getName();
+    });
+    for (const auto &item: Sorted) {
         item.print();
     }
 }
@@ -50,38 +59,44 @@ void Heap<Comparator>::heapSort() {
     heap = temp;
     if (!isMinHeap)
         reverse(sortedItems.begin(), sortedItems.end());
-
     for (auto &item: sortedItems) {
         item.print();
     }
 }
 
 template<typename Comparator>
-void Heap<Comparator>::displaySortedByName(bool ascending) {
+void Heap<Comparator>::displayItems() {
     if (heap.empty())
         return void(cout << "Nothing to display. \n");
-    vector<Item> Sorted = heap;
-    sort(Sorted.begin(), Sorted.end(), [ascending](Item a, Item b) {
-        return ascending ? a.getName() < b.getName() : a.getName() > b.getName();
-    });
-
-    for (const auto &item: Sorted) {
+    for (auto &item: heap) {
         item.print();
     }
 }
 
 template<typename Comparator>
-void Heap<Comparator>::displaySortedByPrice(bool ascending) {
-    if (heap.empty())
-        return void(cout << "Nothing to display. \n");
-    vector<Item> Sorted = heap;
-    sort(Sorted.begin(), Sorted.end(), [ascending](Item a, Item b) {
-        return ascending ? a.getPrice() < b.getPrice() : a.getPrice() > b.getPrice();
-    });
+int Heap<Comparator>::size() {
+    return heap.size();
+}
 
-    for (const auto &item: Sorted) {
-        item.print();
-    }
+template<typename Comparator>
+void Heap<Comparator>::removeItem() {
+    if (size() == 0)
+        return;
+    heap[0] = heap.back();
+    heap.pop_back();
+    heapifyDown(0);
+}
+
+template<typename Comparator>
+void Heap<Comparator>::addItem(const Item &item) {
+    heap.push_back(item);
+    heapifyUp(size() - 1);
+}
+
+template<typename Comparator>
+void Heap<Comparator>::changeHeapType() {
+    isMinHeap = !isMinHeap;
+    buildHeap();
 }
 
 template<typename Comparator>
@@ -96,13 +111,12 @@ void Heap<Comparator>::heapifyDown(int index) {
     int l = leftChild(index);
     int r = rightChild(index);
     int smallestOrLargest = index;
-    if (l < size() and ((isMinHeap and compare(heap[l], heap[index])) ||
-                        (!isMinHeap and !compare(heap[index], heap[l])))) {
+
+    if (l < size() && ((isMinHeap && compare(heap[l], heap[index])) || (!isMinHeap && compare(heap[index], heap[l])))) {
         smallestOrLargest = l;
     }
-
-    if (r < size() and ((isMinHeap and compare(heap[r], heap[smallestOrLargest])) ||
-                        (!isMinHeap and !compare(heap[smallestOrLargest], heap[r])))) {
+    if (r < size() && ((isMinHeap && compare(heap[r], heap[smallestOrLargest])) ||
+                       (!isMinHeap && compare(heap[smallestOrLargest], heap[r])))) {
         smallestOrLargest = r;
     }
 
@@ -114,8 +128,8 @@ void Heap<Comparator>::heapifyDown(int index) {
 
 template<typename Comparator>
 void Heap<Comparator>::heapifyUp(int index) {
-    if (index and ((isMinHeap and compare(heap[parent(index)], heap[index])) ||
-                   (!isMinHeap and !compare(heap[index], heap[parent(index)])))) {
+    if (index && ((isMinHeap && compare(heap[index], heap[parent(index)])) ||
+                  (!isMinHeap && compare(heap[parent(index)], heap[index])))) {
         swap(heap[index], heap[parent(index)]);
         heapifyUp(parent(index));
     }
@@ -203,5 +217,3 @@ int main() {
     }
     return 0;
 }
-
-
