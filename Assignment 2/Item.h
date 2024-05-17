b3b7bc4250af6a5150ca56c0f764272b4f071088
Zeyad Mohamed <zeadmohamed522@gmail.com>
2024-05-17 21:54:00 +0300

    New Updates

--- Assignment 2/Item.h
+++ Assignment 2/Item.h
@@ -6,18 +6,26 @@ using namespace std;
 
 class Item
 {
-public:
-    string itemName;
+private:
+    string name;
     string category;
     int price;
 
+public:
+    Item();
     Item(const string &n, const string &c, int p);
+    void print() const;
+    string getName() const;
+    int getPrice() const;
+    string getCategory() const;
 
     bool operator<(const Item &other) const;
     bool operator>(const Item &other) const;
     bool operator==(const Item &other) const;
+    Item &operator=(const Item &other);
 
     friend ostream &operator<<(ostream &os, const Item &item);
+    friend istream &operator>>(istream &is, Item &item);
 };
 
 #endif // ITEM_H
